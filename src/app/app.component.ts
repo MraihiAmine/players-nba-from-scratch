12251b4d6647e1de8b9c13ea2d0c200286e4fb2c
Amine Mraihi <mraihiamin@gmail.com>
2025-01-01 13:37:06 +0100

    MMD1-Add-search-bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { players } from '../players';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppComponent {
   title = 'players-nba-from-scratch';
+  players = players;
 }
