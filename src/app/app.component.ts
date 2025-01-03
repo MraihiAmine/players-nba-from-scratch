d567b9f42daf81e32b9aabc8230e2a96f92c4455
Amine Mraihi <mraihiamin@gmail.com>
2025-01-03 07:54:05 +0100

    MMD-3 first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { players } from '../players';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  schemas:[NO_ERRORS_SCHEMA]
 })
 export class AppComponent {
   title = 'players-nba-from-scratch';
+  players = players;
 }
